48c3830783d617bba3d001c791f1cd7d18cb1213
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-04-03 05:57:33 +0700

    feat(instruction): create entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 #![allow(unexpected_cfgs)]
 
+use borsh::BorshDeserialize;
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+use crate::{
+    instructions::{CastVoteV1, CreateProposalV1, DaoInstruction, ExecuteProposalV1, InitDaoV1},
+    utils::InstructionProcessor,
 };
 
 mod instructions;
@@ -15,5 +21,18 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    Ok(())
+    let instruction = DaoInstruction::try_from_slice(instruction_data)?;
+
+    match instruction {
+        DaoInstruction::InitDaoV1(data) => {
+            InitDaoV1::try_from((accounts, data, program_id))?.process()
+        }
+        DaoInstruction::CreateProposalV1(data) => {
+            CreateProposalV1::try_from((accounts, data, program_id))?.process()
+        }
+        DaoInstruction::CastVoteV1 => CastVoteV1::try_from((accounts, program_id))?.process(),
+        DaoInstruction::ExecuteProposalV1 => {
+            ExecuteProposalV1::try_from((accounts, program_id))?.process()
+        }
+    }
 }
